3d41fb8c05ca3641ea78c9fa81929d01b8dfb139
Simon Plenderleith <simon@simonplend.co.uk>
2024-08-30 09:15:09 -0700

    Tweaks

--- 01-composing-types-part-1/samples/03-generic-types.ts
+++ 01-composing-types-part-1/samples/03-generic-types.ts
@@ -40,3 +40,89 @@
 
   console.log(spainLanguages.languages.join(", "));
 }
+
+// TODO: More examples
+
+{
+  type MessageOf<MediumType> = MediumType extends { message: unknown }
+    ? MediumType["message"]
+    : never;
+
+  interface Email {
+    message: string;
+  }
+
+  interface SMS {
+    message: string;
+  }
+
+  type EmailMessage = MessageOf<Email>;
+  //   ^?
+
+  type SMSMessage = MessageOf<SMS>;
+  //   ^?
+
+  type NoMessage = MessageOf<number>;
+  //   ^?
+}
+
+// ----
+
+{
+  type MessageOfV2<MediumType> = MediumType extends {
+    message: infer MessageType;
+  }
+    ? MessageType
+    : never;
+
+  interface Email {
+    message: string;
+  }
+
+  interface SMS {
+    message: string;
+  }
+
+  type EmailMessageV2 = MessageOfV2<Email>;
+  //   ^?
+
+  type SMSMessageV2 = MessageOfV2<SMS>;
+  //   ^?
+
+  type NoMessageV2 = MessageOfV2<number>;
+  //   ^?
+}
+
+// ----
+
+{
+  type Photo = {
+    format: "jpeg" | "png";
+    quality: "low" | "high";
+  };
+
+  type EditOptions = {
+    format: "jpeg" | "png";
+    quality: "low" | "high";
+    grayscale: boolean;
+    blur: boolean;
+  };
+
+  type EditPhoto<PhotoType, EditOptionsType> = PhotoType extends {
+    format: infer Format;
+    quality: infer Quality;
+  }
+    ? EditOptionsType extends { format: Format; quality: Quality }
+      ? PhotoType & EditOptionsType
+      : never
+    : never;
+
+  type EditedPhoto = EditPhoto<Photo, EditOptions>;
+
+  const photo: EditedPhoto = {
+    format: "jpeg",
+    quality: "high",
+    grayscale: true,
+    blur: false,
+  };
+}
--- 02-improving-type-safety/lab/README.md
+++ 02-improving-type-safety/lab/README.md
@@ -1,8 +1,10 @@
-# Lab: Type Safe Creators
+# Lab: Type Safe Creators 🎬
+
+> **Section 2: Improving Type Safety**
 
 ## Requirements
 
 1. There are eight pesky type errors in the Step 1 section of `start.ts`. Use the `satisfies` operator to help you fix them.
 2. In the `for...of` loop in Step 2 section, `Array.isArray` is used to narrow the type for the `creator` variable. Refactor this code to use type predicates to narrow the type instead.
     1. Create two type predicate functions: `isDirector()` and `isProducer()`
-    2. Hint: The return type for these functions should be a type predicate where the type is an intersection type.
+    2. Hint: The return type for these functions should be a type predicate where the type is an *intersection type*.
